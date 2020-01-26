3d58ddac3729a39eb3aba69d9aa837dba3bf41e1
Petr Levtonov <petr@levtonov.com>
2020-01-26 19:42:31 +0100

    #7: Fix incorrect shared memory access

--- src/Spork/SharedMemory.php
+++ src/Spork/SharedMemory.php
@@ -1,13 +1,15 @@
 <?php
 
 /*
- * This file is part of Spork, an OpenSky project.
+ * This file is part of the thelevti/spork package.
  *
- * (c) OpenSky Project Inc
+ * (c) Petr Levtonov <petr@levtonov.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
 
 namespace Spork;
 
@@ -49,55 +51,69 @@ class SharedMemory
     /**
      * Reads all messages from shared memory.
      *
-     * @return array An array of messages
+     * @return mixed Any data that has been serialized into the shared memory.
      */
     public function receive()
     {
-        if ($shmId = @shmop_open($this->pid, 'a', 0, 0)) {
-            $serializedMessages = shmop_read($shmId, 0, shmop_size($shmId));
-            shmop_delete($shmId);
-            shmop_close($shmId);
-
-            return unserialize($serializedMessages);
+        if (false === ($shmId = @shmop_open($this->pid, 'a', 0, 0))) {
+            return [];
         }
 
-        return [];
+        /** @var string|false $sharedMemory */
+        $sharedMemory = shmop_read($shmId, 0, 0);
+        if (false === $sharedMemory) {
+            throw new ProcessControlException(sprintf(
+                'Not able to read from shared memory segment for PID: %d',
+                $this->pid
+            ));
+        }
+
+        if (false === shmop_delete($shmId)) {
+            throw new ProcessControlException(sprintf(
+                'Not able to delete shared memory segment for PID: %d',
+                $this->pid
+            ));
+        }
+        shmop_close($shmId);
+
+        return unserialize($this->strFromMem($sharedMemory));
     }
 
     /**
      * Writes a message to the shared memory.
      *
-     * @param mixed          $message The message to send
-     * @param int|null|false $signal  The signal to send afterward. Null to use
-     * @param int            $pause   The number of microseconds to pause after signalling
+     * @param mixed          $message The message to send.
+     * @param int|null|false $signal  The signal to send afterward. Null to use.
+     * @param int            $pause   The number of microseconds to pause after
+     *                                signalling.
      */
     public function send($message, $signal = null, $pause = 500)
     {
-        $messageArray = [];
-
-        if ($shmId = @shmop_open($this->pid, 'a', 0, 0)) {
-            // Read any existing messages in shared memory
-            $readMessage = shmop_read($shmId, 0, shmop_size($shmId));
-            $messageArray[] = unserialize($readMessage);
-            shmop_delete($shmId);
-            shmop_close($shmId);
+        $messages = $this->receive();
+        if (!is_array($message)) {
+            $messages = [
+                $messages,
+            ];
         }
 
         // Add the current message to the end of the array, and serialize it
-        $messageArray[] = $message;
-        $serializedMessage = serialize($messageArray);
+        $messages[] = $message;
+        $serializedMsgs = serialize($messages);
+        $terminatedMsgs = $this->strToMem($serializedMsgs);
+        $termMsgsLen = strlen($terminatedMsgs);
 
         // Write new serialized message to shared memory
-        $shmId = shmop_open($this->pid, 'c', 0644, strlen($serializedMessage));
+        $shmId = shmop_open($this->pid, 'c', 0644, $termMsgsLen);
         if (!is_resource($shmId)) {
             throw new ProcessControlException(sprintf(
-                'Not able to create shared memory segment for PID: %s',
+                'Not able to create shared memory segment for PID: %d',
                 $this->pid
             ));
-        } elseif (shmop_write($shmId, $serializedMessage, 0) !== strlen($serializedMessage)) {
-            throw new ProcessControlException(
-                'Not able to write message to shared memory segment.'
-            );
+        } elseif (shmop_write($shmId, $terminatedMsgs, 0) !== $termMsgsLen) {
+            throw new ProcessControlException(sprintf(
+                'Not able to write to shared memory segment for PID: %d.',
+                $this->pid
+            ));
         }
 
         if (false === $signal) {
@@ -117,5 +133,32 @@ class SharedMemory
         $pid = null === $this->ppid ? $this->pid : $this->ppid;
 
         return posix_kill($pid, $signal);
+    }
+
+    /**
+     * Prepares a string to be stored in memory by appending a terminating zero.
+     *
+     * @param  string $string String to be prepared.
+     * @return string String safe to be directly put in memory.
+     */
+    private function strToMem(string &$string): string
+    {
+        return "{$string}\0";
+    }
+
+    /**
+     * Reads a string from memory by stopping at the first terminating zero.
+     *
+     * @param  string $rawString String from memory.
+     * @return string String ending without the first terminating zero.
+     */
+    private function strFromMem(string &$rawString): string
+    {
+        $pos = strpos($rawString, "\0");
+        if (false === $pos) {
+            return $rawString;
+        }
+
+        return substr($rawString, 0, $pos);
     }
 }
