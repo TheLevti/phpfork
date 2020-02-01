925fc36c81817ccc8bac3b9b0805f261c89fc399
Petr Levtonov <petr@levtonov.com>
2020-02-01 21:34:33 +0100

    #7: Do not execute parent's shutdown function in child process

--- tests/Spork/ProcessManagerTest.php
+++ tests/Spork/ProcessManagerTest.php
@@ -1,13 +1,15 @@
 <?php
 
 /*
- * This file is part of Spork, an OpenSky project.
+ * This file is part of the thelevti/spork package.
  *
- * (c) OpenSky Project Inc
+ * (c) Petr Levtonov <petr@levtonov.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
 
 namespace Spork;
 
